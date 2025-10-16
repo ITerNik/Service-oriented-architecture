516c864955cec7ed3216a1cf734291b2733c191d
ITerNik <ternik2705@list.ru>
2025-10-17 02:55:00 +0300

    build: configured project modules

---
+++ calculating-service/src/main/java/ru/ifmo/calculatingservice/CalculatingServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.ifmo.calculatingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatingServiceApplication.class, args);
+    }
+
+}
