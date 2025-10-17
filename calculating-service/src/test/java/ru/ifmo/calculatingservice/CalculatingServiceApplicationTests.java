816fb492f0d12c393efe001ca59468f14fed22c9
Kirill Zakusov <username918r818@outlook.com>
2025-10-17 11:38:07 +0300

    fixed tests

--- calculating-service/src/test/java/ru/ifmo/calculatingservice/CalculatingServiceApplicationTests.java
+++ calculating-service/src/test/java/ru/ifmo/calculatingservice/CalculatingServiceApplicationTests.java
@@ -1,13 +1,7 @@
 package ru.ifmo.calculatingservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CalculatingServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
