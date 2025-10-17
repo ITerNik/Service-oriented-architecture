adcf3e1903d2d9fc7f6f202a806e5ffc1d9f0bd5
Kirill Zakusov <username918r818@outlook.com>
2025-10-17 13:03:43 +0300

    fix: extend CalculatingServiceApplication to support Spring Boot servlet initialization

--- calculating-service/src/main/java/ru/ifmo/calculatingservice/CalculatingServiceApplication.java
+++ calculating-service/src/main/java/ru/ifmo/calculatingservice/CalculatingServiceApplication.java
@@ -2,12 +2,19 @@ package ru.ifmo.calculatingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class CalculatingServiceApplication {
+public class CalculatingServiceApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CalculatingServiceApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CalculatingServiceApplication.class);
     }
 
 }
