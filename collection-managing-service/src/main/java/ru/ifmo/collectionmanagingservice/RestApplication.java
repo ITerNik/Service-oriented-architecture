72566986719e51ad8bab21c3517e79c946cb363c
Kirill Zakusov <username918r818@outlook.com>
2025-10-17 14:12:14 +0300

    fix: update application path in RestApplication to root

--- collection-managing-service/src/main/java/ru/ifmo/collectionmanagingservice/RestApplication.java
+++ collection-managing-service/src/main/java/ru/ifmo/collectionmanagingservice/RestApplication.java
@@ -3,6 +3,6 @@ package ru.ifmo.collectionmanagingservice;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
-@ApplicationPath("/api")
+@ApplicationPath("/")
 public class RestApplication extends Application {
 }
