7af6f16dff3e0adfd283f44c809845b6c51a9665
Kirill Zakusov <username918r818@outlook.com>
2025-10-18 21:01:38 +0300

    fix: update package name and class name in CalculatingServiceApplicationTests

--- front/src/test/java/ru/ifmo/calculatingservice/CalculatingServiceApplicationTests.java
+++ front/src/test/java/ru/ifmo/calculatingservice/CalculatingServiceApplicationTests.java
@@ -1,7 +1,7 @@
-package ru.ifmo.calculatingservice;
+package ru.ifmo.front;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CalculatingServiceApplicationTests {
+class FrontApplicationTests {
 }
