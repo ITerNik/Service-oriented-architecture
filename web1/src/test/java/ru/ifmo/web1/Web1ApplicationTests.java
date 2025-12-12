a4b095eba30e5d73adcea28611a5757ea18d9073
Kirill Zakusov <username918r818@outlook.com>
2025-12-12 13:33:45 +0300

    feat: init root gradle files

--- web1/src/test/java/ru/ifmo/web1/Web1ApplicationTests.java
+++ web1/src/test/java/ru/ifmo/web1/Web1ApplicationTests.java
@@ -4,10 +4,4 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Web1ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+class Web1ApplicationTests {}
