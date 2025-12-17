123652ea8ba667b0aee7355a11ff9ab2f3f93fba
Kirill Zakusov <username918r818@outlook.com>
2025-12-17 19:56:44 +0300

    fix: remove deprecated annotation

--- web2/src/test/java/ru/ifmo/calculatingservice/controller/RouteControllerTest.java
+++ web2/src/test/java/ru/ifmo/calculatingservice/controller/RouteControllerTest.java
@@ -7,7 +7,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.ifmo.calculatingservice.service.RouteService;
 
@@ -17,7 +17,7 @@ public class RouteControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private RouteService routeService;
 
     @Test
